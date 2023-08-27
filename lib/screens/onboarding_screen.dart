dd17df45ba976574f15b22856233e6963b92bfe7
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2023-08-27 15:51:03 -0300

    part 1 done

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,99 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_app/components/animated_btn.dart';
+import 'package:rive/rive.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation(
+      'active',
+      autoplay: false,
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset('assets/Backgrounds/Spline.png'),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 20,
+                sigmaY: 10,
+              ),
+            ),
+          ),
+          const RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 30,
+                sigmaY: 30,
+              ),
+              child: const SizedBox(),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  const SizedBox(
+                    width: 260,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Learn desing & code',
+                          style: TextStyle(
+                              fontSize: 60, fontFamily: "Poppins", height: 1.2),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.',
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                  AnimatedBtn(
+                    btnAnimationController: _btnAnimationController,
+                    press: () {
+                      _btnAnimationController.isActive = true;
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                        'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, '),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
