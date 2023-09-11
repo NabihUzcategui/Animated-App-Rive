f22dc7ecb74c5ca36b9b1f90ade83c2a4d5533f0
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2023-09-11 20:46:30 -0300

    feature1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_app/components/course_card.dart';
+import 'package:flutter_animated_app/components/secondary_course_card.dart';
 import 'package:flutter_animated_app/models/course.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -58,55 +58,6 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SecondaryCourseCard extends StatelessWidget {
-  final Course course;
-
-  const SecondaryCourseCard({
-    Key? key,
-    required this.course,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-      decoration: BoxDecoration(
-        color: course.bgColor,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(20),
-        ),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Text(
-                  course.title,
-                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.w600),
-                ),
-                const Text(
-                  'Watch video - 15 min',
-                  style: TextStyle(color: Colors.white60, fontSize: 16),
-                )
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 40,
-            child: VerticalDivider(
-              color: Colors.white70,
-            ),
-          ),
-          const SizedBox(width: 8),
-          SvgPicture.asset(course.iconSrc)
-        ],
       ),
     );
   }
